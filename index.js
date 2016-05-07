361a1fe40a79dab776290904dc0f8d5cc48988b4
zfpx <1020355025@qq.com>
2016-05-07 23:30:19 +0800

    -m

---
+++ index.js
@@ -0,0 +1,68 @@
+var main = document.querySelector('#main');
+var oLis = document.querySelectorAll(".slide>li");
+var winW = window.innerWidth;
+var winH = window.innerHeight;
+var desW = 640;
+var desH = 960;
+//var oAudio = document.querySelector("#audio1");
+
+main.style.webkitTransform = "scale(" + winH / desH + ")";
+[].forEach.call(oLis, function () {
+    arguments[0].index = arguments[1];
+    arguments[0].addEventListener('touchstart', start, false);
+    arguments[0].addEventListener('touchmove', move, false);
+    arguments[0].addEventListener('touchend', end, false);
+})
+function start(e) {
+    //oAudio.play();
+    this.startY = e.changedTouches[0].pageY;
+}
+function move(e) {
+    e.preventDefault();
+    /*阻止默认行为*/
+    var touchMove = e.changedTouches[0].pageY;
+    var changePos = touchMove - this.startY;
+    var cur = this.index;
+    var step = 1 / 2;
+    var scalePos = (Math.abs(changePos) / winH) * step;
+    [].forEach.call(oLis, function () {
+        if (arguments[1] != cur) {
+            arguments[0].style.display = "none";
+        }
+        arguments[0].className = "";
+        arguments[0].firstElementChild.id = "";
+    })
+    if (changePos > 0) {/*↓*/
+        var pos = -winH + changePos;
+        this.preSIndex = cur == 0 ? oLis.length - 1 : cur - 1;
+
+    } else if (changePos < 0) {/*↑*/
+        var pos = winH + changePos;
+        this.preSIndex = cur == oLis.length - 1 ? 0 : cur + 1;
+
+    }
+    oLis[this.preSIndex].style.webkitTransform = "translate(0," + pos + "px)";
+    oLis[this.preSIndex].className = "zIndex";
+    oLis[this.preSIndex].style.display = "block";
+    oLis[cur].style.webkitTransform = "scale(" + (1 - scalePos) + ") translate(0," + changePos + "px)";
+}
+function end(e) {
+    oLis[this.preSIndex].style.webkitTransform = "translate(0,0)";
+    oLis[this.preSIndex].style.webkitTransition = "0.5s";
+    oLis[this.preSIndex].addEventListener('webkitTransitionEnd', function () {
+        this.style.webkitTransition = "";
+        this.firstElementChild.id = "a" + (this.index + 1);
+    })
+}
+window.setTimeout(function () {
+    var oList = document.querySelectorAll(".a3>div");
+    for (var i = 0; i < oList.length; i++) {
+        oList[i].className = "move";
+    }
+}, 200);
+
+
+
+
+
+
